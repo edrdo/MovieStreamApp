5a275b5b081a025f941d261bbb6468a9657eb206
Eduardo Marques <edrdo@dcc.fc.up.pt>
2023-12-03 10:12:46 +0000

    cleanup

--- db.py
+++ db.py
@@ -10,8 +10,6 @@ def connect():
   global DB
   c = sqlite3.connect('movie_stream.db', check_same_thread=False)
   c.row_factory = sqlite3.Row
-  # lambda cursor,row:  \
-  #   { col[0]: row[idx] for idx,col in enumerate(cursor.description) }
   DB['conn'] = c
   DB['cursor'] = c.cursor()
   logging.info('Connected to database')
